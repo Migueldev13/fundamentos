dc356b467887da015c68d49736b28c9f07de8f43
Miguel Dev <131427788+Migueldev13@users.noreply.github.com>
2023-05-30 23:06:46 -0700

    Add files via upload

---
+++ console.js
@@ -0,0 +1,25 @@
+
+// documentacao oficial https://nodejs.org/api/errors.html#errors_errors
+//log --> registro :: Embora seja o mais utilizado, .log() é um dos vários métodos que podemos utilizar para exibir informações na chamada “saída padrão” - o terminal - enquanto estamos desenvolvendo uma aplicação. A palavra log significa algo como “registro”, então este método apenas registra no terminal o que passamos entre os parênteses, por exemplo o conteúdo de uma variável ou o resultado de uma operação.
+
+// console.log é usado para colocar dados para fora da nossa aplicacao de uma forma que nao apareça diretamente para o nosso usuario, mas o usuario tem acesso caso ele precise. 
+
+// const minhaVar = true;
+
+// console.log(234)
+// console.log("eu sou um texto")
+// console.log(minhaVar)
+
+
+//Tratamento de erro 
+
+// console.error('Deu erro!')
+
+// Entre os outros métodos, existem:
+
+// console.error() para exibir mensagens de erro;
+// console.table() para visualizar de forma mais organizada informações tabulares;
+// console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+// console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+
+//Documentacao sobre console: https://nodejs.org/api/console.html
