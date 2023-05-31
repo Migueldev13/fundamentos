853cfe15a9668949d9e281ab6625e54dfe3d29f5
Miguel Dev <131427788+Migueldev13@users.noreply.github.com>
2023-05-30 23:07:26 -0700

    Add files via upload

---
+++ comparacoes.js
@@ -0,0 +1,33 @@
+// comparação implicita == compara só o valor 
+
+// const numero = 5;
+// const texto = "5"; 
+// console.log(numero==texto) true
+
+// const numero = 5;
+// const texto = "5"; 
+// console.log(numero===texto) false
+
+// comparação explicita ===  compara o valor e o tipo de dado
+Number()
+String()
+
+
+
+
+
+//typtof
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+
+
+// A lista de operadores é extensa e há muitas formas de utilizá-los. Além das operações matemáticas básicas, do = para atribuição de valor em uma variável e das comparações com == e ===, utilizamos operadores para diversas outras tarefas de código, por exemplo:
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
+
+
+//documentacao expressoes e operadores https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
